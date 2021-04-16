7146b368b706219521792e449dcf9506f218a893
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-04-16 23:48:23 +0200

    Initial commit

---
+++ Main_Logic/__init__.py
@@ -0,0 +1,4 @@
+from functionality import Word
+import sqlite3
+
+_all_ = ["Word", "sqlite3"]
---
+++ Main_Logic/functionality.py
@@ -0,0 +1,24 @@
+class Word:
+    def __init__(self, word):
+        self.word = word
+
+    def add_word(self):
+        pass
+
+    def edit_word(self):
+        pass
+
+    def delete_word(self):
+        pass
+
+
+def data_base_decorator():
+    pass
+
+
+class Manual_Translate:
+    pass
+
+
+class Automatic_Translate:
+    pass
