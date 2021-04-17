37138b15c84830c5d586a2d9280b8e649c943a73
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-04-17 22:35:40 +0200

    Made adding word function

--- Main_Logic/main.py
+++ Main_Logic/main.py
@@ -1,5 +1,11 @@
 from Main_Logic import *
 
+
 @data_base_decorator
 def main(data_base_connection, data_base_cursor):
-    word = Word(data_base_connection, data_base_cursor)
+    word = Word("Nowe słowo", data_base_connection, data_base_cursor)
+    print(word.add_word())
+
+
+if __name__ == '__main__':
+    main()
