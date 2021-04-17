e54871ea6c315cf2272d89610f5768d16e7f8eb2
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-04-17 22:35:40 +0200

    Made adding word function

--- Main_Logic/functionality.py
+++ Main_Logic/functionality.py
@@ -3,21 +3,34 @@ import sqlite3
 
 class Word:
 
-    def __init__(self, word, cursor):
+    def __init__(self, word, connection, cursor):
         """
 
         :param word: Take a word from user
-        :param cursor: Take a cursor from @data_base_decorator argument "cursor"
+        :param connection: Take a connection from data_base_decorator argument "connection"
+        :param cursor: Take a cursor from data_base_decorator argument "cursor"
         """
         self.word = word
         self.cursor = cursor
+        self.connection = connection
 
     def word_valid(self):
         return True
 
-    def add_word(self) -> None:
+    def add_word(self) -> str:
         if self.word_valid():
-            pass
+            with self.connection:
+                self.cursor.execute("INSERT INTO words VALUES (:base_word ,:translated_word)",
+                                    {"base_word": self.word, "translated_word": "Word"})
+
+                self.cursor.execute("SELECT * FROM words WHERE base_word=:base_word",
+                                    {"base_word":self.word})
+
+            return f"Dodano pomyślnie słówko {list(self.cursor.fetchone())[0]}:{list(self.cursor.fetchone())[1]}"
+
+        else:
+
+            return "Nie udało się dodać słówka"
 
     def edit_word(self) -> None:
         pass
@@ -35,14 +48,16 @@ class Automatic_Translate:
 
 
 def data_base_decorator(func):
-    """This decorator open and close connection with sqlite3 data base
-    :param - func, have two arguments it's 'cursor' and 'connection' from data base
+    """This decorator open and close connection with sqlite3 data base \n
+    :param func: Main(Core) function
+    func take 2 arguments connection and cursor from sqlite3 data base
     """
     def wrapper():
         connection = sqlite3.connect("Local_words.db")
         cursor = connection.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS words (base_word TEXT,
                         translated_word TEXT)""")
+
         func(connection, cursor)
         connection.close()
 
