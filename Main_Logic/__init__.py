9b5a3529ecb85202bac7d0f2631e0e11898cc4fc
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-06-01 00:11:08 +0200

    Rename functionality, repair edit_language and add display_word_list.

--- Main_Logic/__init__.py
+++ Main_Logic/__init__.py
@@ -1,6 +1,6 @@
 from constans import Base, Engine, Session
 from models import Word, Language
-from functionality import Manual_Translate, Automatic_Translate
+from main_functions import Manual_Translate, Automatic_Translate
 from operation_on_data_base import create_tables, create_languages, data_base_decorator
 
 _all_ = ["Base", "Engine", "Word", "Session", "Word", "Language", "Manual_Translate", "Automatic_Translate",
--- Main_Logic/test_functionality.py
+++ Main_Logic/test_functionality.py
@@ -26,7 +26,7 @@ class TestManual_Translate(TestCase):
         cls.session.add_all([cls.first_lang, cls.second_lang, cls.third_lang])
         pl_record = cls.session.query(Language).filter(Language.name == "Polski").first()
         en_record = cls.session.query(Language).filter(Language.name == "Angielski").first()
-        de_record = cls.session.query(Language).filter(Language.name == "Niemiecki").first()
+        cls.session.query(Language).filter(Language.name == "Niemiecki").first()
 
         cls.manual = Manual_Translate(pl_record.id, en_record.id)
         cls.automatic = Automatic_Translate(pl_record.id, en_record.id)
@@ -36,9 +36,13 @@ class TestManual_Translate(TestCase):
         base_word = "Jaśko".title()
         translated_word = "Bogdanowicz".title()
 
+        base_word2 = "Janko".title()
+        translated_word2 = "Zbyszko".title()
+
         self.assertEqual(self.manual.add_word(base_word, translated_word), [base_word, translated_word])
-    #
-    # def test_b_automatic_add_word(self):
+        self.assertEqual(self.manual.add_word(base_word2, translated_word2), [base_word2, translated_word2])
+
+    # def test_b_automatic_add_word(self) -> None:
     #
     #     base_word = "Jabłko".title()
     #
@@ -50,15 +54,19 @@ class TestManual_Translate(TestCase):
 
         self.assertEqual(self.manual.edit_word(old_value, new_value), [old_value, new_value])
 
-    def test_d_edit_language(self):
+    def test_d_edit_language(self) -> None:
         old_language = "Polski".title()
-        new_language = "Angielski".title()
+        new_language = "Niemiecki".title()
         word_id = 1
 
         self.assertEqual(self.manual.edit_language(word_id, old_language, new_language),
                          [old_language, new_language])
 
-    def test_e_delete_word(self) -> None:
+    def test_e_display_words_list(self) -> None:
+
+        self.assertIsInstance(self.manual.display_words_list(), dict)
+
+    def test_f_delete_word(self) -> None:
         data_for_del = "Bogdan".title()
 
         self.assertEqual(self.manual.delete_word(data_for_del), data_for_del)
